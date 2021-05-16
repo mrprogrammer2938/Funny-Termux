131e3af5483098da24f79cae426e9962b4d7d10b
Ms.programmer <78996423+msprogrammer2938@users.noreply.github.com>
2021-05-16 01:50:47 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# This code write by Ms.nope
+clear
+echo "installing..."
+sleep 2
+echo "
+
+█▀▀ █░░█ █▀▀▄ █▀▀▄ █░░█ ░░ ▀▀█▀▀ █▀▀ █▀▀█ █▀▄▀█ █░░█ █░█ 
+█▀▀ █░░█ █░░█ █░░█ █▄▄█ ▀▀ ░▒█░░ █▀▀ █▄▄▀ █░▀░█ █░░█ ▄▀▄ 
+▀░░ ░▀▀▀ ▀░░▀ ▀░░▀ ▄▄▄█ ░░ ░▒█░░ ▀▀▀ ▀░▀▀ ▀░░░▀ ░▀▀▀ ▀░▀"
+echo ""
+sudo apt install python3
+sudo apt install python
+chmod +x funnny
+echo ""
+echo "finish installing!"
+echo ""
+echo "usage:
+      ./funny"
+exit 1
